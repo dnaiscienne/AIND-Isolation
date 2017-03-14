ea6101475e87ec161d412d2f0034c0fcc165904a
dnaiscienne <dnaiscienne@yahoo.com>
2017-03-15 00:23:37 +0800

    begun rough outline of get_move function

--- game_agent.py
+++ game_agent.py
@@ -119,24 +119,47 @@ class CustomPlayer:
         self.time_left = time_left
 
         # TODO: finish this function!
-
-        # Perform any required initializations, including selecting an initial
+		
+		# Perform any required initializations, including selecting an initial
         # move from the game board (i.e., an opening book), or returning
         # immediately if there are no legal moves
 
+		
+		# Assigns  (-1, -1) as default representing no available legal moves
+		best_move = (-1, -1)
+		
+		# Checks if there are still legal moves. If there are no legal moves return (-1, -1)
+		if not legal_moves:
+		    return best_move
+		
+		# First move :  Return center position. Assumes that the board is a square and that there is a center (odd square)
+		if game.move_count in (0, 1):
+		    return (game.height//2, game.width//2)
+		
+		# Get the method to be used. Minimax or Alpha-beta
+		method = self.minimax if self.method=='minimax' else self.alphabeta
+        
         try:
             # The search method call (alpha beta or minimax) should happen in
             # here in order to avoid timeout. The try/except block will
             # automatically catch the exception raised by the search method
             # when the timer gets close to expiring
-            pass
+			if self.iterative:
+			    
+			
+			
+            else:
+			# Select best move based on evaluation
+			    for move in legal_moves:
+				    branch_score, branch_move  = method()
+			
 
         except Timeout:
             # Handle any actions required at timeout, if necessary
             pass
 
         # Return the best move from the last completed search iteration
-        raise NotImplementedError
+        return best_move
 
     def minimax(self, game, depth, maximizing_player=True):
         """Implement the minimax search algorithm as described in the lectures.
@@ -173,7 +196,7 @@ class CustomPlayer:
             raise Timeout()
 
         # TODO: finish this function!
-        raise NotImplementedError
+        return 
 
     def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf"), maximizing_player=True):
         """Implement minimax search with alpha-beta pruning as described in the
